f061415a5360aab2ba475b853b4fb8e8b8e7fea5
jandwally <jwalli@sas.upenn.edu>
2018-11-26 16:37:17 -0500

    Testing file added, for us to test by randomized cross-validation

--- predict_labels.m
+++ predict_labels.m
@@ -1,15 +1,9 @@
 function pred_labels=predict_labels(train_inputs,train_labels,test_inputs)
-% part = repmat(1:2,1,ceil(size(train_inputs,1)/2));
-% part = part(1:size(train_inputs,1));
-% part = part(randperm(size(train_inputs,1)));
-% test_labels = train_labels(part==2,:);
-% test_inputs = train_inputs(part==2,:);
-% train_labels = train_labels(part==1,:);
-% train_inputs = train_inputs(part==1,:);
-pred_labels = zeros(size(test_inputs,1),size(train_labels,2));
-for i = 1:size(train_labels,2)
-    Mdl = fitrlinear(train_inputs, train_labels(:,i));
-    pred_labels(:,i) = predict(Mdl, test_inputs);
-end
-% error_metric(pred_labels,test_labels)
+
+	pred_labels = zeros(size(test_inputs,1),size(train_labels,2));
+	for i = 1:size(train_labels,2)
+	    Mdl = fitrlinear(train_inputs, train_labels(:,i));
+	    pred_labels(:,i) = predict(Mdl, test_inputs);
+	end
+
 end
