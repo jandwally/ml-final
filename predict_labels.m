1696c8769ed3771c40b4e643ce3db4274bd614ed
Nikita Lapin <31903981+nlapin1@users.noreply.github.com>
2018-12-01 23:28:48 -0500

    woop

--- predict_labels.m
+++ predict_labels.m
@@ -1,38 +1,58 @@
 function pred_labels=predict_labels(train_inputs,train_labels,test_inputs)
 
-	%{
-	Health outcomes are:
-	    1: 'health_aamort'    -- years potential life lost, per 100000
-	    2: 'health_fairpoor'  -- % of adults that report fair or poor health
-	    3: 'health_mentunh'   -- average # of mentally unhealthy days
-	    4: 'health_pcdiab'    -- % of adults reported diabetic
-	    5: 'health_pcexcdrin' -- % of adults who drink excessively
-	    6: 'health_pcinact'   -- % of adults who report no leisure physical activity
-	    7: 'health_pcsmoker'  -- % of adults who are smokers
-	    8: 'health_physunh'   -- average # of physically unhealthy days
-	    9: 'heath_pcobese'	  -- % of adults obese
-	%}
-
-	pred_labels = zeros(size(test_inputs,1),size(train_labels,2));
-
-	% 1: 'health_aamort'
-	model1 = fitrlinear(train_inputs, train_labels(:,1));
-	pred_labels(:,1) = predict(model1, test_inputs);
-
-	% 2: 'health_fairpoor'
-	% 3: 'health_mentunh'
-	model3 = fitrlinear(train_inputs, train_labels(:,3));
-	pred_labels(:,3) = predict(model3, test_inputs);
-
-	% 4: 'health_pcdiab'
-	% 5: 'health_pcexcdrin'
-	% 6: 'health_pcinact'
-	% 7: 'health_pcsmoker'
-	% 8: 'health_physunh'
-	model8 = fitrlinear(train_inputs, train_labels(:,8));
-	pred_labels(:,8) = predict(model8, test_inputs);
-
-	% 9: 'heath_pcobese'
-	
-
+part = repmat(1:7,1,ceil(size(train_inputs,1)/7));
+part = part(1:size(train_inputs,1));
+part = part(randperm(size(train_inputs,1)));
+pred_labels = zeros(size(test_inputs,1),size(train_labels,2));
+C_vals = [0, 1e-4, 1e-3, 1e-2, 1e-1, 1];
+for f = 1:9
+    min_idx = 0; min_cv_err = Inf;
+    for i = 1:6
+        C = C_vals(i);
+        tot_cv_err = 0;
+        for j = 1:7
+            curlbls = train_labels(:,f);
+            X_train = train_inputs(part~=i,:);
+            y_train = curlbls(part~=i,:);
+            X_test = train_inputs(part==i,:);
+            y_test = curlbls(part==i,:);
+            % setting up the net and training
+            net = fitnet(10);
+            net.layers{1}.transferFcn = 'poslin';
+            net.performFcn = 'mse';
+            net.performParam.regularization = C;
+            net.trainFcn = 'trainlm';
+            net = configure(net, X_train', y_train');
+            net = train(net, X_train', y_train');
+            % predicting
+            y_pred = net(X_test')';
+            cl_err = sum(sqrt(sum((y_test-y_pred).^2)));
+            tot_cv_err = tot_cv_err + cl_err;
+        end
+        if tot_cv_err < min_cv_err
+            min_cv_err = tot_cv_err;
+            min_idx = i;
+        end
+    end
+    C_min = C_vals(min_idx)
+    X_train = train_inputs;
+    y_train = train_labels(:,f);
+    X_test = test_inputs;
+    % setting up and training final net w optimal parameters
+    net = fitnet(10);
+    net.layers{1}.transferFcn = 'poslin';
+    net.performFcn = 'mse';
+    net.performParam.regularization = C_min;
+    net.trainFcn = 'trainlm';
+    net = configure(net, X_train', y_train');
+    net = train(net, X_train', y_train');
+    % predicting
+    pred_labels(:,f) = net(X_test')';
+end
+% y_p_tr = sign(net(X_train')');
+% fprintf('C used by final model: %f\n', C_min);
+% fprintf('training error achieved by final model: %f\n', ...
+%     classification_error(y_p_tr, y_train));
+% fprintf('min cross validation error across all parameters: %f\n', ...
+%     min_cv_err/5);
 end
--- test.m
+++ test.m
@@ -1,23 +1,20 @@
-
-% Use this file to test if our methods work, so we don't need to modify predict_labels.m
-
 function err = test(training_data, training_labels)
 
-    n = size(training_data, 1)
-    p = size(training_data, 2)
-    d = size(training_labels, 2)
+n = size(training_data, 1);
+p = size(training_data, 2);
+d = size(training_labels, 2);
 
-    % Get two folds to test with
-    part = cross_validation(n, 2)
-    actual_labels = training_labels(part==2,:);
-    testing_data = training_data(part==2,:);
-    training_labels = training_labels(part==1,:);
-    training_data = training_data(part==1,:);
+% Get two folds to test with
+part = cross_validation(n, 10);
+actual_labels = training_labels(part==1,:);
+testing_data = training_data(part==1,:);
+training_labels = training_labels(part~=1,:);
+training_data = training_data(part~=1,:);
 
-    % Call a method
-    pred_labels = predict_labels(training_data, training_labels, testing_data)
+% Call a method
+pred_labels = predict_labels(training_data, training_labels, testing_data);
 
-    % Compute cross-validation error
-    error_metric(pred_labels, actual_labels)
+% Compute cross-validation error
+error_metric(pred_labels, actual_labels)
 
 end
